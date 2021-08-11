0dcace5c1690be830a1355bb83bc13ed7d73483a
mpogorelova <mdpogorelova@edu.hse.ru>
2021-08-11 22:19:33 +0300

    parser, client, server are refactored, logging deleted

--- main.py
+++ main.py
@@ -1,76 +1,100 @@
-# This is a sample Python script.
+"""
+Это клиент-серверное приложение
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import logging
 import socket
-import sys
+from pyparsing import Word, nums, ZeroOrMore, Optional, oneOf
 
 
-def client(host_client, port_client, flag_server, log):
-    if flag_server in "-t":
-        sock_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock_c.connect((host_client, port_client))
-        message = input('Введите сообщение')
-        sock_c.send(bytes(message, encoding='UTF-8'))
-        data = sock_c.recv(1024)
-        print(data)
-        sock_c.close()
+# 127.0.0.1 13000 -s -t -f log_output
+def parser(variable):
+    """
+    Это парсер, который выполняет разбор команды клиента
+    """
+    port_name = (Word(nums))('port_entered')
+    host_name = Word(nums)
+    full_host_name = (host_name + ZeroOrMore('.' + host_name))('host_entered')
+    flag1 = (Optional('-s'))('mode_flag')
+    flag2 = (Optional('-' + oneOf('u t')))('delivery_flag')
+    flag3 = ('-' + oneOf('o f'))('log_flag')
+    log_output = Optional('log_output')
+    parse_module = full_host_name + port_name + flag1 + flag2 + flag3 + log_output
+    res = parse_module.parseString(variable)
+    host_entered = ''.join(res.host_entered)
+    port_entered = res.port_entered
+    mode_flag = res.mode_flag
+    delivery_flag = ''.join(res.delivery_flag)
+    log_flag = ''.join(res.log_flag)
+    return host_entered, port_entered, mode_flag, delivery_flag, log_flag
 
 
-def server(host_server, port_server, flag_server, log):
-    if flag_server in "-u":
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            log.info("Socket open UDP")
-        except UnboundLocalError:
-            log.error("UnboundLocalError: ")
-            sys.exit(1)
+def server(host_var, port_var, deliv):
+    """
+    Это серверная часть приложения
+    """
+    host = host_var
+    port = int(port_var)
+    tcp_or_udp = deliv
+    if tcp_or_udp == '-u':
+        while 1:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print('UDP-сокет создан')
+            sock.bind((host, port))
+            data = sock.recvfrom(1024)
+            received = data[0]
+            addr = data[1]
+            info = str(addr)
+            print('подсоединился:', addr)
+            sock.sendto(bytes(info, encoding='UTF-8'),addr)
+            sock.close()
     else:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('TCP-сокет создан')
+        sock.bind((host, port))
+        print('Сервер готов слушать')
+        sock.listen()
+        while 1:
+            conn, addr = sock.accept()
+            data2 = str(addr)
+            print('подсоединился:', addr)
+            data = conn.recv(1024)
+            print(str(data))
+            conn.send(bytes(data2, encoding='UTF-8'))
+            conn.close()
+
+
+def client(host_var, port_var, deliv):
+    """
+    Это клиентская часть приложения
+    """
+    host = host_var
+    port = int(port_var)
+    tcp_or_udp = deliv
+    if tcp_or_udp == '-u':
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        log.info("Socket open TCP")
-    sock.bind((host_server, port_server))
-    logging.info("Server get host and port")
-    sock.listen()
-    while 1:
-        conn, addr = sock.accept()
-        data2 = str(addr)
-        print('подсоединился:', addr)
-        data = conn.recv(1024)
-        print(str(data))
-        conn.send(bytes(data2, encoding='UTF-8'))
-        logging.info("Message send")
-        conn.close()
-        logging.info("Socket close")
-
-
-def parser(string_parser):
-    host_parser = string_parser.split(' ')[0]
-    port_parser = string_parser.split(' ')[1]
-    flag_parser = string_parser.split(' ').copy()
-    return host_parser, port_parser, flag_parser
-
-
-def get_logger(flags_log):
-    if flags_log in "-f":
-        log = logging.getLogger()
-        fh = logging.FileHandler(flags_log[-1], encoding='utf-8')
-        log.addHandler(fh)
+        print('UDP-сокет создан')
+        message = input('Введите сообщение')
+        sock.sendto(bytes(message, encoding='UTF-8'),(host, port))
+        data = sock.recvfrom(1024)
+        print(data[0])
+        sock.close()
     else:
-        log = logging.getLogger()
-        sh = logging.StreamHandler(stream=sys.stdout)
-        log.addHandler(sh)
-    return log
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('TCP-сокет создан')
+        sock.connect((host, port))
+        message = input('Введите сообщение')
+        sock.send(bytes(message, encoding='UTF-8'))
+        data = sock.recv(1024)
+        print(data)
+        sock.close()
 
 
-# Press the green button in the gutter to run the script.
-# <host> <port> [-s] [-t | -u] [-o | -f <file>]
+
 if __name__ == '__main__':
-    input_string = input()
-    host, port, flags = parser(input_string)
-    logger = get_logger(flags)
-    if flags in "-s":
-        server(host, port, flags, logger)
+    cmd = input()
+    host_ent, port_ent, mode, delivery, log = parser(cmd)
+    print(host_ent, port_ent, mode, delivery, log)
+    if mode == '-s':
+        server(host_ent, port_ent, delivery)
     else:
-        client(host, port, flags, logger)
+        client(host_ent, port_ent, delivery)
